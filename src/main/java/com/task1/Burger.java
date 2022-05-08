fb788d3643fc9be26589e9d6eb58d1951b684a64
Ilya <skatehunt@gmail.com>
2022-05-08 15:12:39 +0300

    v4

--- src/main/java/com/task1/Burger.java
+++ src/main/java/com/task1/Burger.java
@@ -1,4 +1,53 @@
 package com.task1;
 
 public class Burger {
+    boolean hasBun;
+    boolean hasMeat;
+    boolean hasCheese;
+    boolean hasGreenery;
+    boolean hasMayonnaise;
+    boolean hasSecondMeat;
+
+    public Burger(boolean hasBun, boolean hasMeat, boolean hasCheese,
+                  boolean hasGreenery, boolean hasMayonnaise) {
+        this.hasBun = hasBun;
+        this.hasMeat = hasMeat;
+        this.hasCheese = hasCheese;
+        this.hasGreenery = hasGreenery;
+        this.hasMayonnaise = hasMayonnaise;
+        if (hasBun && hasMeat && hasCheese && hasGreenery && hasMayonnaise) {
+            System.out.println("Recipe of common burger: bun x1, meat x1, cheese x1, greenery x1, mayonnaise x1");
+            System.out.println();
+        }
+
+    }
+
+    public Burger(boolean hasBun, boolean hasMeat, boolean hasCheese,
+                  boolean hasGreenery, boolean hasMayonnaise, boolean hasSecondMeat) {
+        this.hasBun = hasBun;
+        this.hasMeat = hasMeat;
+        this.hasCheese = hasCheese;
+        this.hasGreenery = hasGreenery;
+        this.hasMayonnaise = hasMayonnaise;
+        this.hasSecondMeat = hasSecondMeat;
+        if (hasBun && hasMeat && hasCheese && hasGreenery && hasMayonnaise && hasSecondMeat) {
+            System.out.println("Recipe of double-meat burger: bun x1, meat x2, cheese x1, greenery x1, mayonnaise x1");
+            System.out.println();
+        }
+
+    }
+
+    public Burger(boolean hasBun, boolean hasMeat, boolean hasCheese, boolean hasGreenery) {
+        this.hasBun = hasBun;
+        this.hasMeat = hasMeat;
+        this.hasCheese = hasCheese;
+        this.hasGreenery = hasGreenery;
+        if (hasBun && !hasMeat && hasCheese && hasGreenery) {
+            System.out.println("Recipe of diet burger: bun x1, meat x1, cheese x1, greenery x1");
+            System.out.println();
+        }
+
+    }
+
+
 }
--- src/main/java/com/task1/BurgerMain.java
+++ src/main/java/com/task1/BurgerMain.java
@@ -1,4 +1,14 @@
 package com.task1;
 
 public class BurgerMain {
+    public static void main(String[] args) {
+        Burger burger1 = new Burger(true, true, true,
+                true, true);
+        Burger burger2 = new Burger(true, false, true,
+                true);
+        Burger burger3 = new Burger(true, true, true,
+                true, true, true);
+
+
+    }
 }
